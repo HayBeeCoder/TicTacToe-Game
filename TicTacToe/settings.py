a60d4e92e5034891822207788ecaa604103ef5a5
Mustapha Ahmad <18-30gc052@students.unilorin.edu.ng>
2022-08-12 22:57:47 +0100

    Deploying

--- TicTacToe/settings.py
+++ TicTacToe/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 SECRET_KEY = config('SECRET_KEY')
@@ -29,6 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
+ALLOWED_HOSTS = ["*", "tictactoedra.herokuapp.com"]
 # Application definition
 
 INSTALLED_APPS = [
